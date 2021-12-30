edc07531ebd0c337eeed1de275e4f7eb53ffcf98
Malik Saifullah <saif@coredirection.com>
2021-12-30 15:57:25 +0500

    adding overly and change image

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-if ($('.member-login, .corporate-wellness, .event-consultancy').hasClass('.member-login, .corporate-wellness, .event-consultancy')) {
-    $(this).addClass('opacity');
-} else {
-    $(this).removeClass('opacity');
-}
-
 $(document).ready(function () {
     $(".member-login, .corporate-wellness, .event-consultancy").hover(function () {
         $(this).toggleClass("overlay");
+        $(".overlay").addClass("opacity");
+        $(this).removeClass("opacity");
     });
 });
